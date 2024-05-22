b52f04dad4d5a22ccc35b7063a2a4ea5148bbe74
Zhasur Sidamatov <sidamatov.offwork@gmail.com>
2024-05-22 23:06:41 +0300

    Initial Commit

---
+++ AuthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthApp
+//
+//  Created by Zhasur Sidamatov on 22.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
