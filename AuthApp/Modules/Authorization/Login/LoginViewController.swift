6a4ce886e5f038062bcb27a492ced772895c45d7
Zhasur Sidamatov <sidamatov.offwork@gmail.com>
2024-05-24 13:43:10 +0300

    Implement modules

---
+++ AuthApp/Modules/Authorization/Login/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  AuthApp
+//
+//  Created by Zhasur Sidamatov on 24.05.2024.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController  {
+    
+    private lazy var backgroundImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: <#T##String#>)
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupConstraints()
+    }
+}
+
+private extension LoginViewController {
+    func setupViews() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+}
